962ef921a11d7ff100f1340a61fe034cbfcf060c
TaeGaram (Office) <drasla217@gmail.com>
2026-01-22 17:31:22 +0900

    auth 관련 Jwt 인증 삭제

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,7 +5,6 @@ import { authenticateJwt } from "../middlewares/auth.middleware";
 const authRoute = Router();
 const authController = new AuthController();
 
-authRoute.use(authenticateJwt);
 authRoute.post('/register', authController.register);
 authRoute.post('/login', authController.login);
 
