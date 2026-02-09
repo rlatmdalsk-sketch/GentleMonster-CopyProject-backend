6bcdd1ec66445c38523bbafd6c565f7e69d8b882
Tae.Garam (Office) <drasla217@gmail.com>
2026-02-09 10:54:42 +0900

    주문 목록 조회에 페이지 및 제한 값 처리 로직 추가

--- src/controllers/admin.order.controller.ts
+++ src/controllers/admin.order.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AdminOrderService } from "../services/admin.order.service";
+import { GetAdminOrderListQuery } from "../schemas/admin.order.schema";
 
 const adminOrderService = new AdminOrderService();
 
@@ -7,7 +8,15 @@ export class AdminOrderController {
     // 전체 주문 조회
     async getAllOrders(req: Request, res: Response, next: NextFunction) {
         try {
-            const query = req.query as any;
+            const page = Number(req.query.page) || 1;
+            const limit = Number(req.query.limit) || 20;
+
+            const query = {
+                ...req.query,
+                page,
+                limit,
+            } as unknown as GetAdminOrderListQuery;
+
             const result = await adminOrderService.getAllOrders(query);
             res.status(200).json({ message: "조회 성공", data: result });
         } catch (error) {
